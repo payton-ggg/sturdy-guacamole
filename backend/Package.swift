66da17b26f15289e91c8a786d728e4fcefa64244
Platon <platon20081019@gmail.com>
2024-11-04 22:50:48 +0200

    Add backend and frontend folders

---
+++ backend/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+let package = Package(
+    name: "ChatApp",
+    platforms: [
+        .macOS(.v13)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0")
+    ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")
+            ],
+            path: "Sources/App"
+        ),
+        .executableTarget(
+            name: "Run",
+            dependencies: ["App"],
+            path: "Sources"
+        )
+    ]
+)
